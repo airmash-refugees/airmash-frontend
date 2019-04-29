254bad2f1011a579c7c16ecf995fbe6a22cc1a31
Derps <44081022+derps-airmash@users.noreply.github.com>
2019-04-29 17:24:32 +0100

    Hacky/racy best-effort attempt at setting country flag.
    This is because /game is a static file right now.

---
+++ js/derps-geoip-shim.js
@@ -0,0 +1,14 @@
+
+/**
+ * Fetch our GeoIP from geojs.io (free service), since /game is currently a
+ * static file.
+ */
+$(function()
+{
+    $.get('https://get.geojs.io/v1/ip/country.json', null, function(response)
+    {
+        if(response && response.country) {
+            window.game.myFlag = 'NL';
+        }
+    });
+});
