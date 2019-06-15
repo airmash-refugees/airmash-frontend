065219335282c0bf6fae311070615f4c52afbbff
Derps <44081022+derps-airmash@users.noreply.github.com>
2019-06-15 18:51:38 +0100

    update shim

--- js/derps-geoip-shim.js
+++ js/derps-geoip-shim.js
@@ -5,6 +5,13 @@
  */
 $(function()
 {
+    if(! Cookies.get('cid')) {
+        Cookies.set('cid', Math.floor(Math.random()*1e16).toString(36), {
+            expires: 3650,
+            domain: 'airmash.online'
+        });
+    }
+
     $.get('https://get.geojs.io/v1/ip/country.json', null, function(response)
     {
         if(response && response.country) {
