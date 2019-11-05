180dc467acaab19e28b6448f8a8f776623def4d5
congratulatio <45519482+congratulatio@users.noreply.github.com>
2019-11-05 23:33:57 +0000

    Added scaling factor modification warning for EU FFA 2

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,15 @@ import './Sound';
 import './Textures';
 import './Tools';
 import './UI';
+
+// this is a temporary hack until we have a standard way of having the game server communicate its maximum scaling factor
+!function() {
+    var alreadyWarned = false;
+    setInterval(function() {
+        if (!alreadyWarned && config.scalingFactor > 2500 && "eu" == game.playRegion && "ffa2" == game.playRoom)
+        {
+            console.log("%c⚠️ The maximum scalingFactor on FFA #2 is 2500. This is enforced by the server. For zoom hacking, switch to FFA #1.", "color:red");
+            alreadyWarned = true;
+        }
+    }, 100);
+}();   
