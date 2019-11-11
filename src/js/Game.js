d6d682e5d7e3e11a6cc21ca2159aad43e0f699a3
derps <54752088+derpsderps69@users.noreply.github.com>
2019-11-11 23:30:15 +0000

    Game: reformat ships array and add comments for each element

--- src/js/Game.js
+++ src/js/Game.js
@@ -71,52 +71,65 @@ window.game = {
 window.config = {
     storage: {},
     settings: {},
-    ships: [{}, {
-        name: "raptor",
-        turnFactor: .065,
-        accelFactor: .225,
-        maxSpeed: 5.5,
-        minSpeed: .001,
-        brakeFactor: .025,
-        energyLight: .6,
-        collisions: [[0, 5, 23], [0, -15, 15], [0, -25, 12]]
-    }, {
-        name: "spirit",
-        turnFactor: .04,
-        accelFactor: .15,
-        maxSpeed: 3.5,
-        minSpeed: .001,
-        brakeFactor: .015,
-        energyLight: .9,
-        collisions: [[0, 0, 35], [50, 14, 16], [74, 26, 14], [30, 8, 23], [63, 22, 15], [-50, 14, 16], [-74, 26, 14], [-30, 8, 23], [-63, 22, 15]]
-    }, {
-        name: "mohawk",
-        turnFactor: .07,
-        accelFactor: .275,
-        maxSpeed: 6,
-        minSpeed: .001,
-        brakeFactor: .025,
-        energyLight: .3,
-        collisions: [[0, -12, 15], [0, 0, 17], [0, 13, 15], [0, 26, 15]]
-    }, {
-        name: "tornado",
-        turnFactor: .055,
-        accelFactor: .2,
-        maxSpeed: 4.5,
-        minSpeed: .001,
-        brakeFactor: .025,
-        energyLight: .5,
-        collisions: [[0, 8, 18], [14, 12, 13], [-14, 12, 13], [0, -12, 16], [0, -26, 14], [0, -35, 12]]
-    }, {
-        name: "prowler",
-        turnFactor: .055,
-        accelFactor: .2,
-        maxSpeed: 4.5,
-        minSpeed: .001,
-        brakeFactor: .025,
-        energyLight: .75,
-        collisions: [[0, 11, 25], [0, -8, 18], [19, 20, 10], [-19, 20, 10], [0, -20, 14]]
-    }],
+    ships: [
+        // Unused (0)
+        {},
+        // PlaneType.Predator (1)
+        {
+            name: "raptor",
+            turnFactor: .065,
+            accelFactor: .225,
+            maxSpeed: 5.5,
+            minSpeed: .001,
+            brakeFactor: .025,
+            energyLight: .6,
+            collisions: [[0, 5, 23], [0, -15, 15], [0, -25, 12]]
+        },
+        // PlaneType.Goliath (2)
+        {
+            name: "spirit",
+            turnFactor: .04,
+            accelFactor: .15,
+            maxSpeed: 3.5,
+            minSpeed: .001,
+            brakeFactor: .015,
+            energyLight: .9,
+            collisions: [[0, 0, 35], [50, 14, 16], [74, 26, 14], [30, 8, 23], [63, 22, 15], [-50, 14, 16], [-74, 26, 14], [-30, 8, 23], [-63, 22, 15]]
+        },
+        // PlaneType.Mohawk (3)
+        {
+            name: "mohawk",
+            turnFactor: .07,
+            accelFactor: .275,
+            maxSpeed: 6,
+            minSpeed: .001,
+            brakeFactor: .025,
+            energyLight: .3,
+            collisions: [[0, -12, 15], [0, 0, 17], [0, 13, 15], [0, 26, 15]]
+        },
+        // PlaneType.Tornado (4)
+        {
+            name: "tornado",
+            turnFactor: .055,
+            accelFactor: .2,
+            maxSpeed: 4.5,
+            minSpeed: .001,
+            brakeFactor: .025,
+            energyLight: .5,
+            collisions: [[0, 8, 18], [14, 12, 13], [-14, 12, 13], [0, -12, 16], [0, -26, 14], [0, -35, 12]]
+        },
+        // PlaneType.Prowler (5)
+        {
+            name: "prowler",
+            turnFactor: .055,
+            accelFactor: .2,
+            maxSpeed: 4.5,
+            minSpeed: .001,
+            brakeFactor: .025,
+            energyLight: .75,
+            collisions: [[0, 11, 25], [0, -8, 18], [19, 20, 10], [-19, 20, 10], [0, -20, 14]]
+        }
+    ],
     /*
      * This array contains elements whose index is a missile MobType and whose
      * value is an object with keys:
